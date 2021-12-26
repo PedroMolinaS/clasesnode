e0a7380856dca1de100733b6b70eb43b09ed194d
Administrator <admin@example.com>
2021-12-26 14:02:35 -0500

    probando express

--- clase_express/app.js
+++ clase_express/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const port = 8080
 
-app.get('/', function (req, res) {
-  res.send('Hola Pedro')
+app.get('/', (req, res) => {
+  res.send('Home Page')
 })
 
-app.listen(3000)
+app.get('/jelou', (req, res) => {
+  res.send('Jelou jelou- XP')
+})
+
+app.get('*', (req, res) => {
+  res.send('404 | Page not found')
+}) 
+
+
+app.listen(port, () => {
+  console.log(`App corriendo en: http://localhost:${port}`)
+})
