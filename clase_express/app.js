0fc49a195f6e0cd4d565b3742899cab05af38bd4
Administrator <admin@example.com>
2021-12-27 09:33:02 -0500

    pantallas html

--- clase_express/app.js
+++ clase_express/app.js
@@ -10,8 +10,12 @@ app.use(express.static('public'))
 //   res.send('Home Page')
 // })
 
-app.get('/jelou', (req, res) => {
-  res.send('Jelou jelou- XP')
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
 })
 
 app.get('*', (req, res) => {
